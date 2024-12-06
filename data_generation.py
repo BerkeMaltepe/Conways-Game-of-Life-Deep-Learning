e703520ced40d55878a4d0aab275611518d13d86
Berke Maltepe <34270476+BerkeMaltepe@users.noreply.github.com>
2024-12-05 22:43:26 -0500

    make data generation

--- data_generation.py
+++ data_generation.py
@@ -18,7 +18,7 @@ def generate_data(randomData):
 
     data = [board]
 
-    for _ in range(200):
+    for _ in range(255):
         newboard = F.conv2d(board, weights, padding=1).view(32, 32)
         newboard = (newboard==12) | (newboard==3) | (newboard==13)
         newboard_array = np.int8(newboard) * 255 
@@ -27,7 +27,21 @@ def generate_data(randomData):
         print(len(data))
 
     torch.save(data, 'train.data')
+
+
+    last_frame = None
+    newdata = []
+    for i, d in enumerate(data):
+        if i == 0:
+            last_frame = d
+            continue
+        if abs(d.sum().item()) > 1.5 * abs(last_frame.sum().item()):
+            pass
+        else:
+            newdata.append((last_frame, d))
+        last_frame = d
+
+    torch.save(newdata, 'train.data.batch')
     print("Data saved")
-    
 
 
